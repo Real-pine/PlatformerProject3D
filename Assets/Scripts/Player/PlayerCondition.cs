eb12922886953bb5db01934a4c16979d3706061c
Real-pine <crewger9030@gmail.com>
2024-10-29 17:46:18 +0900

    feat: 체력UI구현
    PlayerCondition과 UICondition, Condition을 통해 체력UI구현

--- Assets/Scripts/Player/PlayerCondition.cs
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerCondition : MonoBehaviour
+public interface IDamagable
 {
-    // Start is called before the first frame update
-    void Start()
+    void TakePhysicalDamage(int damageAmount);
+}
+
+public class PlayerCondition : MonoBehaviour , IDamagable
+{
+    public UICondition uiCondition;
+    
+    Condition health { get {return uiCondition.health;}}
+
+    public event Action onTakeDamage;
+
+    private void Update()
     {
-        
+        // if (health.curValue <= 0.0f)
+        // {
+        //     Die();
+        // }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Heal(float amount)
     {
-        
+        health.Add(amount);
+    }
+
+    private void Die()
+    {
+        Debug.Log("dead");
+    }
+
+    public void TakePhysicalDamage(int damageAmount)
+    {
+        health.Subtract(damageAmount);
+        onTakeDamage?.Invoke();
     }
 }
